a31786df11fa0c2a42565ca8ab11394c185bac66
BS <bs@BSs-Mac-Pro.local>
2018-11-18 21:33:15 +0100

    adjust the mobile version for all pages

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b35a9e25f9966e0320fb";
+/******/ 	var hotCurrentHash = "3299a2cce01176ec0daf";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- README.md
+++ README.md
@@ -3,5 +3,8 @@ This is a Front end test
 
 Just for fun i've completed the layout for the 4 pages.
 
-Maybe i'll add data feching and behaviour to it later
+Maybe i'll add data feching and behaviour later. 👍
+
+
+
 
