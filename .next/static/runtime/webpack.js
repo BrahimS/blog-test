fba76e5aa11cafdbb31659dea0f575fdb43d584c
BS <bs@BSs-Mac-Pro.local>
2018-11-22 14:20:45 +0100

    Make the heart clickable

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a27d82089fa45418db50";
+/******/ 	var hotCurrentHash = "acf2537f471676f103f3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/Heart.js
+++ components/Heart.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Heart = () => {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg" 
-			viewBox="0 0 57.947 57.947"
-			>
-			<path d="M28.947 56.486c15.685-11.277 23.532-21.592 27.222-29.46 4.311-9.193.561-20.589-8.845-24.413C36.268-1.88 28.947 8.486 28.947 8.486S21.678-1.907 10.623 2.588C1.217 6.412-2.533 17.808 1.778 27.001c3.69 7.867 11.484 18.209 27.169 29.485z" fill="#e64c3c"/>
-		</svg>
-	)
+class Heart extends Component {
+	constructor () {
+		super ()
+		this.state = {
+			grey : true
+		}
+	}
+
+handleClick (event)  {
+		event.preventDefault()
+		this.setState({ grey: !this.state.grey })
+	}
+	render () {
+		let heartColor = this.state.grey ? "Heart_grey" : "Heart_red"
+		return (
+			<svg 
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 57.947 57.947"
+				onClick={ this.handleClick.bind(this) }
+				>
+				<path className={heartColor} d="M28.947 56.486c15.685-11.277 23.532-21.592 27.222-29.46 4.311-9.193.561-20.589-8.845-24.413C36.268-1.88 28.947 8.486 28.947 8.486S21.678-1.907 10.623 2.588C1.217 6.412-2.533 17.808 1.778 27.001c3.69 7.867 11.484 18.209 27.169 29.485z" />
+			</svg>
+		)
+	}
+
+
 }
 
 export default Heart
