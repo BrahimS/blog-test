efcd60b2931e8805f7b9c7fcbc4a7495645f319e
BS <bs@BSs-Mac-Pro.local>
2018-11-24 19:09:31 +0100

    merge to master

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9174f4237632e4525204";
+/******/ 	var hotCurrentHash = "670ecd218ca7809e36c0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
